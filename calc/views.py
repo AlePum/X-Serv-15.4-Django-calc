641db958036913ff2b0d5e22ea9fd67eeab83fc7
AlePum <tatotatotato46@gmail.com>
2016-03-21 18:34:40 +0100

    ejercicio

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def sumar(request, num1, num2):
+    suma = int(num1) + int(num2)
+    return HttpResponse("<b><h1>" + "Sumamos: " + str(num1) + " + " +
+                        str(num2) + " = " + str(suma)+ "</h1></b>")
+
+def restar(request, num1, num2):
+    resta = int(num1) - int(num2)
+    return HttpResponse("<b><h1>" + "Restamos: " + str(num1) + " - " +
+                        str(num2) + " = " + str(resta)+ "</h1></b>")
+
+def multiplicar(request, num1, num2):
+    mult = int(num1) * int(num2)
+    return HttpResponse("<b><h1>" + "Multiplicamos: " + str(num1) + " * " +
+                        str(num2) + " = " + str(mult)+ "</h1></b>")
+
+def dividir(request, num1, num2):
+    div = int(num1) / int(num2)
+    return HttpResponse("<b><h1>" + "Dividimos: " + str(num1) + " / " +
+                        str(num2) + " = " + str(div)+ "</h1></b>")
+
+def error(request):
+    return HttpResponse('<b><h1><font color="red">404 Not Found</font></h1></b>')
--- project/urls.py
+++ project/urls.py
@@ -6,5 +6,11 @@ urlpatterns = patterns('',
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+
+    url(r'(\d+)\+(\d+)$', "calc.views.sumar"),
+    url(r'(\d+)-(\d+)$', "calc.views.restar"),
+    url(r'(\d+)\*(\d+)$', "calc.views.multiplicar"),
+    url(r'(\d+)/(\d+)$', "calc.views.dividir"),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'.*', "calc.views.error"),
 )
